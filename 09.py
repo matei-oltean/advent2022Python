287196a48b05351dcf511633cd5294a354ed1027
Matei Oltean <matei.oltean@gmail.com>
2022-12-09 11:49:11 +0100

    Add day09 2

--- 09.py
+++ 09.py
@@ -10,16 +10,23 @@ def adjacent(x1, y1, x2, y2):
     return max(abs(x1-x2), abs(y1-y2)) <= 1
 
 
-def move(xt, yt, xh, yh, seen):
-    while not adjacent(xt, yt, xh, yh):
-        xt, yt = xt + sign(xh - xt), yt + sign(yh - yt)
-        seen.add((xt, yt))
-    return xt, yt
+def move(knots, seen):
+    moved = True
+    while moved:
+        moved = False
+        for i in range(1, len(knots)):
+            (xt, yt), (xh, yh) = knots[i], knots[i-1]
+            if not adjacent(xt, yt, xh, yh):
+                moved = True
+                knots[i] = xt + sign(xh - xt), yt + sign(yh - yt)
+            if i == 1:
+                seen[0].add((xt, yt))
+            elif i == 9:
+                seen[1].add((xt, yt))
 
 
-x, y, xt, yt = 0, 0, 0, 0
-visited = set()
-visited.add((0, 0))
+knots = [(0, 0)]*10
+visited = [set(), set()]
 with (open("09", "r")) as f:
     for line in f.read().splitlines():
         d, dist = line.split(" ")
@@ -33,6 +40,8 @@ with (open("09", "r")) as f:
                 dx, dy = 0, 1
             case "D":
                 dx, dy = 0, -1
-        x, y = x + dx*int(dist), y + dy*int(dist)
-        xt, yt = move(xt, yt, x, y, visited)
-print(len(visited))
+        x, y = knots[0]
+        knots[0] = x + dx*int(dist), y + dy*int(dist)
+        move(knots, visited)
+print(len(visited[0]))
+print(len(visited[1]))
