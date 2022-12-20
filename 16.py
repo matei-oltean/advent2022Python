833d1bf21302a503edfb556714351ec692e9df8e
Matei Oltean <matei.oltean@gmail.com>
2022-12-20 14:23:51 +0100

    Add day16 02

--- 16.py
+++ 16.py
@@ -7,15 +7,12 @@ def flatten(flows, neigh):
         if flow == 0 and node != "AA":
             continue
         res[node] = []
-        seen = set()
-        seen.add(node)
+        seen = {node}
         dist = 1
         frontier = set(neigh[node])
         while frontier:
             new_frontier = set()
-            for n in frontier:
-                if n in seen:
-                    continue
+            for n in frontier.difference(seen):
                 seen.add(n)
                 if flows[n] != 0:
                     res[node].append((n, dist))
@@ -27,29 +24,38 @@ def flatten(flows, neigh):
     return res
 
 
-def open_flows(m, flows):
-    def open_flow(remaining, node, visited, res):
+def open_flows(m, flows, start_timer, with_elephant, memo, node_map):
+    def open_flow(remaining, node, visited, res, has_reset):
+        if has_reset:
+            if (remaining, node, visited) in memo:
+                return res + memo[remaining, node, visited]
         res += flows[node]*remaining
         res2 = res
-        new_visited = visited.copy()
-        new_visited.add(node)
+        new_visited = visited + node_map[node]
         for neigh, dist in m[node]:
-            if neigh not in visited and remaining - dist > 0:
-                res2 = max(res2, open_flow(
-                    remaining - dist, neigh, new_visited, res))
+            if not node_map[neigh] & visited:
+                if remaining - dist > 0:
+                    res2 = max(res2, open_flow(
+                        remaining - dist, neigh, new_visited, res, has_reset))
+                if not has_reset:
+                    res2 = max(res2, open_flow(
+                        start_timer, "AA", new_visited, res, True))
+        if has_reset:
+            memo[remaining, node, visited] = res2 - res
         return res2
-    return open_flow(30, "AA", set(), 0)
+    return open_flow(start_timer, "AA", 0, 0, not with_elephant)
 
 
-flows = {}
-neigh = {}
-
+flows, neigh = {}, {}
 with (open("16", "r")) as f:
     for line in f.read().splitlines():
         node, rate, tunnels = re.search(
             r"Valve ([A-Z]{2}) has flow rate=(\d*); tunnels? leads? to valves? (\D*)", line).groups()
-        tunnels = tunnels.split(', ')
-        rate = int(rate)
-        flows[node] = rate
-        neigh[node] = tunnels
-print(open_flows(flatten(flows, neigh), flows))
+        flows[node] = int(rate)
+        neigh[node] = tunnels.split(', ')
+memo = {}
+flattened = flatten(flows, neigh)
+node_map = {node: 1 << i if node !=
+            "AA" else 0 for i, node in enumerate(flattened)}
+print(open_flows(flattened, flows, 30, False, memo, node_map))
+print(open_flows(flattened, flows, 26, True, memo, node_map))
